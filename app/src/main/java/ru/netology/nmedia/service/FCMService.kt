90296cb6c43bfc92b75aa07e6643e8e752baa170
SpaceCadet <spacecadet2702@gmail.com>
2021-09-05 22:03:19 +0500

    process recipientId

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -4,11 +4,17 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import ru.netology.nmedia.R
 import ru.netology.nmedia.auth.AppAuth
+import java.lang.RuntimeException
+import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
     private val content = "content"
@@ -30,11 +36,33 @@ class FCMService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-        // TODO: replace this in homework
-        println(message.data["content"])
+        try {
+            println(message.data["content"])
+            val push = gson.fromJson(message.data["content"], Push::class.java)
+            when (push.recipientId) {
+                AppAuth.getInstance().authStateFlow.value.id, null -> handlePush(push)
+                else -> AppAuth.getInstance().sendPushToken()
+            }
+            handlePush(push)
+        } catch (e: JsonSyntaxException) {
+            Log.e("FCMService", "can not parse: ${message.data["content"]}")
+        }
+    }
+
+    private fun handlePush(push: Push) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentText(push.content)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
     }
 
     override fun onNewToken(token: String) {
         AppAuth.getInstance().sendPushToken(token)
     }
 }
+
+data class Push(val recipientId: Long?, val content: String)
